2b9ecbad4cda517c0c588d058034e8ad04301834
kaliv0 <kaloyan.ivanov88@gmail.com>
2022-10-03 16:36:02 +0300

    initial commit

---
+++ myth-rest-api/src/main/java/com/kaliv/myths/MythRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.kaliv.myths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MythRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MythRestApiApplication.class, args);
+	}
+
+}

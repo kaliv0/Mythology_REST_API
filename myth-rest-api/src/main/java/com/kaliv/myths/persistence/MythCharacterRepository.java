06a342aad051f14a3c2b06713198cb2e060d806f
kaliv0 <kaloyan.ivanov88@gmail.com>
2023-01-21 13:46:55 +0200

    added queryDSL library

--- myth-rest-api/src/main/java/com/kaliv/myths/persistence/MythCharacterRepository.java
+++ myth-rest-api/src/main/java/com/kaliv/myths/persistence/MythCharacterRepository.java
@@ -1,9 +1,11 @@
 package com.kaliv.myths.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import com.kaliv.myths.entity.MythCharacter;
 
-public interface MythCharacterRepository extends JpaRepository<MythCharacter, Long> {
+public interface MythCharacterRepository extends JpaRepository<MythCharacter, Long>,
+        QuerydslPredicateExecutor<MythCharacter> {
     boolean existsByName(String name);
 }

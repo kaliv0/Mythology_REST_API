3145605217a9bc4572e923a55ebcc6b1c6389780
kaliv0 <kaloyan.ivanov88@gmail.com>
2023-02-04 10:00:15 +0200

    fixed security config

--- myth-rest-api/src/main/java/com/kaliv/myths/config/SecurityConfig.java
+++ myth-rest-api/src/main/java/com/kaliv/myths/config/SecurityConfig.java
@@ -12,6 +12,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().permitAll();
+                .antMatchers("/**")
+                .permitAll()
+                .and()
+                .cors().and().csrf().disable();
     }
 }

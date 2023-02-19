dff075f57619681c1f73a912d3810f3087184b05
kaliv0 <kaloyan.ivanov88@gmail.com>
2023-02-19 14:16:14 +0200

    fixed email validation

--- myth-rest-api/src/main/java/com/kaliv/myths/util/validator/email/EmailValidator.java
+++ myth-rest-api/src/main/java/com/kaliv/myths/util/validator/email/EmailValidator.java
@@ -6,14 +6,19 @@ import javax.validation.ConstraintValidatorContext;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.querydsl.core.util.StringUtils;
+
 import static com.kaliv.myths.constant.EmailConstants.VALID_EMAIL_PATTERN;
 
 public class EmailValidator implements ConstraintValidator<ValidEmail, String> {
     private static final Pattern PATTERN = Pattern.compile(VALID_EMAIL_PATTERN, Pattern.CASE_INSENSITIVE);
 
     @Override
-    public boolean isValid(final String username, final ConstraintValidatorContext context) {
-        return validateEmail(username);
+    public boolean isValid(final String email, final ConstraintValidatorContext context) {
+        if (StringUtils.isNullOrEmpty(email)) {
+            return true;
+        }
+        return validateEmail(email);
     }
 
     private boolean validateEmail(final String email) {
